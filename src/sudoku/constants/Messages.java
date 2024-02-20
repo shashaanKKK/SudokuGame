e1ade633b692f23606035498015564ed6263bfba
shashaanKKK <shasang2610415@gmail.com>
2024-02-20 19:40:45 +0530

    first commit

---
+++ src/sudoku/constants/Messages.java
@@ -0,0 +1,6 @@
+package sudoku.constants;
+
+public class Messages {
+    public static final String GAME_COMPLETE= "CONGRATULATIONS, YOU HAVE WON THE GAME";
+    public static final String ERROR = "AN ERROR HAS OCCURRED";
+}
